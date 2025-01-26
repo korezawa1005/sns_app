c3b363a69fd0cd6849b9b944426ecb2673c48be9
korezawa1005 <t.k.oshihiki@gmail.com>
2025-01-26 20:40:05 +0900

    いいね機能実装①

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :likers, through: :likes, source: :user
   validates :title, presence: true # タイトルは必須
   validates :content, presence: true # 内容も必須
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy # Userとlikesとの直接的なアソシエーション.ユーザーが「どのLikeを作成したか」を直接扱うための定義
+  has_many :liked_posts, through: :likes, source: :post # liked_posts UserとPostモデルの間接的な関係.ユーザーが「どの投稿をいいねしたか」を扱うための定義。
+  # through: :likes likesテーブルを経由して、UserとPostの関係を定義します source: :post likesテーブルのpostという関連を経由してPostモデルを参照することを指定
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
